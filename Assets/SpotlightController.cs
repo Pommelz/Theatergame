3ed1cee743c03633ba84bc28ff8d29656917dd55
DESKTOP-C9JDDR9\Pommelz <s.gsaenger@gmx.de>
2020-10-24 01:33:13 +0200

    added prefabs for spotlight, musicmanager and cheeringmanager

---
+++ Assets/SpotlightController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotlightController : MonoBehaviour
+{
+
+    #region Singleton
+    static SpotlightController instance;
+    public static SpotlightController Instance
+    {
+        get
+        {
+            if (!instance)
+            {
+                Debug.LogError("[SCENECONTROLLER]: i'm the instance, i do not exist.");
+            }
+
+            return instance;
+        }
+    }
+    private void Awake()
+    {
+        if (instance && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+    #endregion
+
+    public Light[] spotlights;
+
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null) return;
+        FollowObject(target);
+    }
+
+    public void SetSpotlightsActive(bool _SetActive, int _amount)
+    {
+        for (int i = 0; i < spotlights.Length; i++)
+        {
+            if (i <= _amount)
+                spotlights[i].enabled = _SetActive;
+        }
+    }
+
+    public void FollowObject(Transform _cameraTarget)
+    {
+        for (int i = 0; i < spotlights.Length; i++)
+        {
+            spotlights[i].transform.LookAt(_cameraTarget);
+        }
+    }
+}
---
+++ Assets/_Scripts/UX/CheerHandler.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheerHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+   
+
+    [SerializeField] private AudioClip[] CheerVariants;
+    private AudioSource[] audios;
+
+    private void Start()
+    {
+        SetupAudioStructure();
+    }
+
+    public void PlayRandomCheer()
+    {
+        int random = Random.Range(0, CheerVariants.Length);
+        PlayAudioOnFirstFreeAvailable(CheerVariants[random].name, CheerVariants[random]);
+    }
+
+    private void SetupAudioStructure()
+    {
+        audios = new AudioSource[2];
+        for (int i = 0; i < audios.Length; i++)
+        {
+            audios[i] = this.gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    private void PlayAudioOnFirstFreeAvailable(string _name, AudioClip myClip)
+    {
+        if (!audios[0].isPlaying)
+        {
+            if (audios[0].clip != audios[0].clip) audios[0].clip = myClip;
+            audios[0].Play();
+        }
+        else if (audios[0].isPlaying && !audios[1].isPlaying)
+        {
+            if (audios[1].clip != audios[1].clip) audios[1].clip = myClip;
+            audios[1].Play();
+        }
+        else
+        {
+            Debug.Log(_name + " could not been played on this Object: " + this.gameObject.name);
+        }
+    }
+
+}
