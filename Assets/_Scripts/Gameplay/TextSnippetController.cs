747bb974c266979b2125ea2e632dc6355f2fca54
Maxi <maxi.karnstedt@googlemail.com>
2020-10-24 14:07:21 +0200

    started implementing Core textbook flow

--- Assets/_Scripts/Gameplay/TextSnippetController.cs
+++ Assets/_Scripts/Gameplay/TextSnippetController.cs
@@ -4,77 +4,174 @@ using UnityEngine;
 using Collections.ListWrapping;
 using UnityEngine.UI;
 using TMPro;
+using Collections.Enums;
 
 public class TextSnippetController : MonoBehaviour
 {
-    public List<ListWrapper> rhyme = new List<ListWrapper>();
-    public List<string> wrongAnswers = new List<string>();
+    public List<AnswerListWrapper> rhyme = new List<AnswerListWrapper>();
+    public List<EmotionAnswerWrapper> wrongAnswers = new List<EmotionAnswerWrapper>();
+
+    public List<string> HappyAnswers = new List<string>();
+    public List<string> SadAnswers = new List<string>();
+    public List<string> BananaAnswers = new List<string>();
 
     public Transform SnippetBttn;
     ObjectPool bttnPool;
 
-    private ListWrapper activeRhyme;
-    private List<string> chosenText = new List<string>();
+    private AnswerListWrapper activeRhyme;
+    private List<EmotionAnswerWrapper> chosenText = new List<EmotionAnswerWrapper>();
     private List<Transform> currBttns = new List<Transform>();
     private int roundCount = 0;
+
+    private Smiley HappyResponse;
+    private Smiley SadResponse;
+    private Smiley BananaResponse;
 
     void Start()
     {
         bttnPool = new ObjectPool(SnippetBttn.gameObject, 3, true);
-        StartRhymeRound();
+        StartRound();
     }
 
-    private void StartRhymeRound()
+    private void StartRound()
     {
         int randomRhyme = Random.Range(0, rhyme.Count);
         activeRhyme = rhyme[randomRhyme];
-        SpawnSnippetBttns(roundCount);
+        RomeoText();
+        //SpawnSnippetBttns(roundCount);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RomeoText();
+        }
     }
 
     private void SpawnSnippetBttns(int round)
     {
         int r = Random.Range(0, 3);
 
+        //TODO: struct
+        EmotionAnswerWrapper wrong1 = null;
+        EmotionAnswerWrapper wrong2 = null;
+
+        EmotionAnswerWrapper julietResponse = rhyme[roundCount].julietText;
+        List<EmotionAnswerWrapper> answers = new List<EmotionAnswerWrapper>();
+        //answers.Add(julietResponse);
+
+        switch (julietResponse.responseSmiley)
+        {
+            case Smiley.HAPPY:
+                wrong1 = SpawnWrongBttnByEmotion(Smiley.SAD);
+                wrong2 = SpawnWrongBttnByEmotion(Smiley.BANANA);
+                break;
+            case Smiley.SAD:
+                wrong1 = SpawnWrongBttnByEmotion(Smiley.HAPPY);
+                wrong1 = SpawnWrongBttnByEmotion(Smiley.BANANA);
+                break;
+            case Smiley.BANANA:
+                wrong1 = SpawnWrongBttnByEmotion(Smiley.HAPPY);
+                wrong2 = SpawnWrongBttnByEmotion(Smiley.SAD);
+                break;
+        }
+
+        answers.Add(wrong1);
+        answers.Add(wrong2);
+        int randomJuliet = Random.Range(0, 3);
+
         for (int i = 0; i < 3; i++)
         {
+
             GameObject bttn = bttnPool.NextFree();
             TextSnippet ts = bttn.GetComponent<TextSnippet>();
-
             currBttns.Add(bttn.transform);
 
-            if (i == r)
+            if (i == randomJuliet)
             {
                 ts.isRhyme = true;
-                bttn.transform.GetChild(0).GetComponent<TextMeshProUGUI>().SetText(activeRhyme.myList[round]);
+                ts.myWrapper = julietResponse;
+                bttn.transform.GetChild(0).GetComponent<TextMeshProUGUI>().SetText(ReturnSmileyStringByEmotion(julietResponse.responseSmiley));
             }
             else
             {
                 ts.isRhyme = false;
-                int randomWrongAnswer = Random.Range(0, wrongAnswers.Count);
-                bttn.transform.GetChild(0).GetComponent<TextMeshProUGUI>().SetText(wrongAnswers[randomWrongAnswer]);
-                ts.wrongAnsweriD = randomWrongAnswer;
+                bttn.transform.GetChild(0).GetComponent<TextMeshProUGUI>().SetText(ReturnSmileyStringByEmotion(answers[0].responseSmiley));
+                ts.myWrapper = answers[0];
+                answers.Remove(answers[0]);
 
             }
+            //ts.SetBttnText();
         }
         roundCount++;
+        //RomeoText();
     }
 
-    public void RoundEvaluation(bool _correctAnswer, int wrongAnswerID)
+    private void RomeoText()
     {
-        chosenText.Add(_correctAnswer ? activeRhyme.myList[roundCount - 1] : wrongAnswers[wrongAnswerID]);
+        string text = rhyme[roundCount].RomeoText;
+        Debug.Log(text);
 
+        //TODO: spawn snippets on romeo animation event
+        SpawnSnippetBttns(roundCount);
+    }
+
+    public string ReturnSmileyStringByEmotion(Smiley _emotion)
+    {
+        string smileystring = "";
+        switch (_emotion)
+        {
+            case Smiley.HAPPY:
+                smileystring = ":)";
+                break;
+            case Smiley.SAD:
+                smileystring = ":(";
+                break;
+            case Smiley.BANANA:
+                smileystring = ":P";
+                break;
+        }
+        return smileystring;
+    }
+
+    private EmotionAnswerWrapper SpawnWrongBttnByEmotion(Smiley _wantedEmotion)
+    {
+        EmotionAnswerWrapper chosen = null;
+        foreach (EmotionAnswerWrapper saw in wrongAnswers)
+        {
+            if (saw.responseSmiley == _wantedEmotion)
+            {
+                chosen = saw;
+                break;
+            }
+        }
+        if (chosen == null)
+        {
+            Debug.Log("chosen null");
+        }
+        //wrongAnswers.Remove(chosen);
+        return chosen;
+    }
+
+    public void RoundEvaluation(bool _correctAnswer, EmotionAnswerWrapper _smileyanswerwrapper)
+    {
+        chosenText.Add(_smileyanswerwrapper);
+        Debug.Log(_correctAnswer ? "correct" + _smileyanswerwrapper.responsePart : "wrong");
         foreach (Transform t in currBttns)
         {
             t.gameObject.SetActive(false);
         }
 
+        currBttns.Clear();
         //TODO: Call this function after the actor is done
         if (roundCount < 3)
-            SpawnSnippetBttns(roundCount);
+            RomeoText();
         else
-            foreach (string s in chosenText)
+            foreach (EmotionAnswerWrapper s in chosenText)
             {
-                Debug.Log(s);
+                Debug.Log(s.responsePart);
             }
+        //RomeoText();
     }
 }
