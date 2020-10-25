f964cac0d771aa0d20a0d094ccb6864b5ffb8d9d
DESKTOP-C9JDDR9\Pommelz <s.gsaenger@gmx.de>
2020-10-25 13:00:45 +0100

    added synchro, tested

--- Assets/_Scripts/UX/SynchronizationHandler.cs
+++ Assets/_Scripts/UX/SynchronizationHandler.cs
@@ -1,23 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SynchronizationHandler : MonoBehaviour
 {
     [SerializeField] AudioSource romeo;
-    [SerializeField] AudioSource julia;
+    [SerializeField]private AudioSource julia;
 
-    
-
-    // Start is called before the first frame update
-    void Start()
+    private void OnEnable()
     {
-        
+        TextSnippetController.OnActorResponse += StartJuliaSynchro;
+        TextSnippetController.OnRomeoAnimation += StartRomeoSynchro;
+        ActorManager.OnActorChanged += SetJuliaSource;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        TextSnippetController.OnActorResponse -= StartJuliaSynchro;
+        TextSnippetController.OnRomeoAnimation -= StartRomeoSynchro;
+        ActorManager.OnActorChanged -= SetJuliaSource;  
     }
+
+    private void StartJuliaSynchro(EmotionAnswerWrapper wrapper)
+    {
+        //if(wrapper.myClip != null)
+        //{
+            julia.clip = wrapper.myClip;
+            julia.Play();
+        //}
+    }
+
+    private void StartRomeoSynchro(AnimationAnswerWrapper romeoanswer)
+    {
+        //if (romeoanswer.myClip != null)
+        //{
+            romeo.clip = romeoanswer.myClip;
+            romeo.Play();
+        //}
+    }
+
+    private void SetJuliaSource(AudioSource source)
+    {
+        julia = source;
+    }
+
+    // Start is called before the first frame update6
 }
