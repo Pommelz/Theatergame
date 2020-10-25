1850b66e92f2ac2f84d60938bfa25c679dc125da
DESKTOP-C9JDDR9\Pommelz <s.gsaenger@gmx.de>
2020-10-25 12:35:34 +0100

    added synchro files, started working on synchro, added fade

--- Assets/CameraSwitch.cs
+++ Assets/CameraSwitch.cs
@@ -35,21 +35,58 @@ public class CameraSwitch : MonoBehaviour
     private Camera activeCamera;
     [SerializeField] private Camera[] availableCameras;
     private CameraFade[] fadeTargets;
+    public float delay;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        fadeTargets = new CameraFade[availableCameras.Length];
+        for (int i = 0; i < fadeTargets.Length; i++)
+        {
+            fadeTargets[i] = availableCameras[i].GetComponent<CameraFade>();
+            Debug.Log(fadeTargets[i]);
+        }
+        activeCamera = Camera.main;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space)) ToggleCamera();
     }
 
-    public ToggleCamera()
+    public void ToggleCamera()
     {
+        if(activeCamera == availableCameras[0])
+        {
+            fadeTargets[0].ToggleFade();
+            fadeTargets[1].ToggleFade();
+            StartCoroutine(StartCameraSwitch());
+        }else if(activeCamera == availableCameras[1])
+        {
+            fadeTargets[0].ToggleFade();
+            fadeTargets[1].ToggleFade();
+            StartCoroutine(StartCameraSwitch());
+        }
+    }
+
+    private IEnumerator StartCameraSwitch()
+    {
+        yield return new WaitForSeconds(delay);
+        if (activeCamera == availableCameras[0])
+        {
+            availableCameras[0].enabled = false;
+            availableCameras[1].enabled = true;
+            activeCamera = availableCameras[1];
+            yield return null;
+        }
+        else if (activeCamera == availableCameras[1])
+        {
+            availableCameras[1].enabled = false;
+            availableCameras[0].enabled = true;
+            activeCamera = availableCameras[0];
+            yield return null;
+        }
 
     }
 }
